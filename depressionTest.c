7000d4ed80795c2c36663cbd603e3ee0184bfca2
chuogirl <60561866+chuogirl@users.noreply.github.com>
2020-02-02 20:19:10 +0800

    Update depressionTest.c

--- depressionTest.c
+++ depressionTest.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-// These headers is for exit(0), took Unix OS into consideration.
 #ifdef _WIN32
 #include <Windows.h>
 #else
@@ -11,9 +10,6 @@
 
 #define char_size 256
 
-/** A function to display menu page
-This is a function to let user choose whether to take the test (or retest as after the test complete it will return to this page) or quit the program
-*/
 int menu()
 {
     char user_choice[256];
@@ -32,17 +28,11 @@ int menu()
 
     printf("\nMenu: \n1. Start Testing \ntype q to quit.\n:");
     scanf("%s", user_choice);
-
-    /** Menu options
-    1 = Start test
-    q = quit
-    other = call menu() again recursively
-    */
     // Start
     if (strcmp(user_choice, "1") == 0)
     {
         // name accept 256 char
-        char user_name, user_gender, user_age;
+        char user_name[char_size], user_gender[1], user_age[3];
         printf("\nPlease enter your name: \n:");
         scanf("%s", user_name);
 
@@ -85,49 +75,49 @@ int menu()
 int depressionTest(char user_gender[])
 {
     int scores[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    char questions_m[][char_size] = {
-        "test1: m",
-        "test2: m",
-        "test3: m",
-        "test4: m",
-        "test5: m",
-        "test6: m",
-        "test7: m",
-        "test8: m",
-        "test9: m",
-        "test10: m",
-        "test11: m",
-        "test12: m",
-        "test13: m",
-        "test14: m",
-        "test15: m",
-        "test16: m",
-        "test17: m",
-        "test18: m",
-        "test19: m",
-        "test20: m",
-    };
     char questions_f[][char_size] = {
-        "test1: f",
-        "test2: f",
-        "test3: f",
-        "test4: f",
-        "test5: f",
-        "test6: f",
-        "test7: f",
-        "test8: f",
-        "test9: f",
-        "test10: f",
-        "test11: f",
-        "test12: f",
-        "test13: f",
-        "test14: f",
-        "test15: f",
-        "test16: f",
-        "test17: f",
-        "test18: f",
-        "test19: f",
-        "test20: f",
+        "test1: I do things slowly. ",
+        "test2: My future seems hopeless.",
+        "test3: It is hard for me to concentrate on reading.",
+        "test4: The pleasure and joy have gone out of my life.",
+        "test5: I have difficulty making decisions.",
+        "test6: I have lost interest in aspects of my life that used to be important to me.",
+        "test7: I feel sad, blue and unhappy.",
+        "test8: I am agitated and keep moving around.",
+        "test9: I feel fatigued.",
+        "test10: It takes great effort for me to do simple things.",
+        "test11: I feel that I am a guilty person who deserves to be punished.",
+        "test12: I feel like a failure.",
+        "test13: I feel lifeless more dead than alive.",
+        "test14: I’m getting too much, too little or not enough restful sleep.",
+        "test15: I spend time thinking about HOW I might kill myself.",
+        "test16: I feel trapped or caught.",
+        "test17: I feel depressed even when good things happen to me.",
+        "test18: Without trying to diet, I have lost or gained weight.",
+        "test19: I feel have trouble concentrating.",
+        "test20: I frequently feel like crying.",
+    };
+    char questions_m[][char_size] = {
+        "test1: Have you dropped many of your activities and interests?",
+        "test2: Do you feel that your life is empty?",
+        "test3: Do you often get bored?",
+        "test4: Are you bothered by thoughts you can’t get out of your head?",
+        "test5: Are you afraid that something bad is going to happen to you?",
+        "test6: Do you often get restless and fidgety?",
+        "test7: Do you prefer to stay at home rather than go out and do things?",
+        "test8: Do you frequently worry about the future?",
+        "test9: Do you feel you have more problems with memory than most?",
+        "test10: Do you feel downhearted and blue?",
+        "test11: Do you feel pretty worthless the way you are now?",
+        "test12: Do you worry a lot about the past?",
+        "test13: Is it hard for you to get started on new projects?",
+        "test14: Do you feel that your situation is hopeless?",
+        "test15: Do you think that most people are better off than you are?",
+        "test16: Do you frequently get upset over little things?",
+        "test17: Do you prefer to avoid social occasions?",
+        "test18: Do you often feel helpless?",
+        "test19: I believe I cause problems for others.",
+        "test20: I don’t have much of an appetite and eat less than usual.",
     };
 
     int questions_arr_size = sizeof(questions_m) / sizeof(questions_m[0]);
@@ -139,7 +129,6 @@ int depressionTest(char user_gender[])
     for (int i = 0; i < questions_arr_size; i++)
     {
         char temp_score[] = {"0"};
-        // if user is male, use questions_m question set, else questions_f
         if (strcmp(user_gender, "m") == 0)
         {
             printf("\n\n%s\n", questions_m[i]);
@@ -148,7 +137,6 @@ int depressionTest(char user_gender[])
         {
             printf("\n\n%s\n", questions_f[i]);
         }
-
         sleep(1);
         while (!(strcmp(temp_score, "1") == 0 ||
                  strcmp(temp_score, "2") == 0 ||
@@ -165,8 +153,8 @@ int depressionTest(char user_gender[])
             }
         }
 
-        // store the user choice to the respective scores array item
         scores[i] = atoi(temp_score);
+        printf("%o", scores[i]);
     }
 
     // Process data
@@ -182,16 +170,19 @@ int outputResults(int average)
     switch (average)
     {
     case 1:
-        printf("BAD BAD");
+        printf("No Depression Likely.\n You do not appear to be experiencing any depressive symptoms at this time.\n If this changes, you may need to seek further diagnosis and treatment.");
         break;
     case 2:
-        printf("NOT BAD");
+        printf("Borderline Depression.\n You appear to be experiencing some very mild depressive symptoms, many of which are commonly found amongst the general population, but border on the possibility of a depressive episode.\n It is unclear whether or not you suffer these problems severely enough to need to seek further diagnosis and treatment.
+");
         break;
     case 3:
-        printf("GOOD");
+        printf("Mild-Moderate Depression.\n You appear to be suffering from mild to moderate depressive symptoms commonly associated with depressive disorders.\n These symptoms appear to be causing you some noticeable signs of impairment and distress,\n and you would likely benefit from the attention of a trained mental health professional for further evaluation and a more accurate diagnosis.
+");
         break;
     case 4:
-        printf("VERY GOOD");
+        printf("Severely Depressed.\n You appear to be suffering from severe depressive symptoms commonly associated with serious depressive disorders,\n such as major depressive disorder, bipolar disorder or dysthymia.\n These symptoms seem to be causing you some serious impairment and distress in your normal, everyday functioning.\n You would likely benefit from the immediate attention of your physician or a trained mental health professional for further evaluation\n and a more accurate diagnosis and treatment.
+");
         break;
     default:
         break;
@@ -211,6 +202,6 @@ int averageScore(int scores[], int arr_size)
 
 void main()
 {
-    // Display Menu Page()
+    // Menu
     menu();
 }
